f9d07c18f9ded54c367d9b0dfd0c11f30fc9a7f8
Ömer Oktay Gültekin <oktay.gultekin@ug.bilkent.edu.tr>
2022-11-14 05:45:08 +0300

    jwt expires in added

--- src/models/user.js
+++ src/models/user.js
@@ -106,7 +106,7 @@ userSchema.methods.toJSON = function () {
 
 userSchema.methods.generateAuthToken = async function() {
     const user = this
-    const token = jwt.sign({_id: user._id.toString()}, process.env.JWT_SECRET)
+    const token = jwt.sign({_id: user._id.toString()}, process.env.JWT_SECRET, { expiresIn: '1h' })
 
     user.tokens = user.tokens.concat({token})
     await user.save()
