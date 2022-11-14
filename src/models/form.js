95f38841db50dfc51ccb42716443b92d6b88bcb7
mertunlu <69141853+mertunlu@users.noreply.github.com>
2022-11-15 00:14:36 +0300

    form models -- only incoming wish list updated

---
+++ src/models/form.js
@@ -0,0 +1,164 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const Task = require('./task')
+
+const formSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    surname: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    active: {
+        type: Boolean,
+        default: false
+    },
+    departments:[{
+        department: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            ref: 'User'
+        },
+        //0 major, 1 minor
+        type: {
+            type: Number,
+            default: 0
+        }
+    }],
+
+    appliedInstitution: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'University'
+    },
+
+    studentId: {
+        type: Number,
+        default: 0
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if(!validator.isEmail(value)) {
+                throw new Error('Invalid email!')
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        validate(value) {
+            if(!(value.length > 6 && !(value.toLowerCase().includes("password")))) {
+                throw new Error('Invalid password')    
+            }
+        }
+    },
+    userType: {
+        type: Number,
+        default: 0,
+        validate(value) {
+            if(value < 0) {
+                throw new Error('User Type must be greater than zero')
+            }
+        }
+    },
+    tokens: [{
+        token: {
+            type: String,
+            required: true
+        }
+    }],
+    avatar: {
+        type: Buffer
+    }
+}, {
+    timestamps: true
+})
+
+// not stored in db for mongoose
+userSchema.virtual('tasks', {
+    ref: 'Task',
+    localField: '_id',
+    foreignField: 'owner'
+})
+
+/*
+userSchema.virtual('university', {
+    ref: 'University',
+    localField: '_id',
+    foreignField: 'owner'
+})
+*/
+
+userSchema.methods.toJSON = function () {
+    const user = this
+    const userObject = user.toObject()
+
+    delete userObject.password
+    delete userObject.tokens
+    delete userObject.avatar
+
+    return userObject
+}
+
+userSchema.methods.generateAuthToken = async function() {
+    const user = this
+    const token = jwt.sign({_id: user._id.toString()}, process.env.JWT_SECRET, { expiresIn: '1h' })
+
+    user.tokens = user.tokens.concat({token})
+    await user.save()
+
+    return token
+}
+
+userSchema.statics.findByCredentials = async (email, password) => {
+    const user = await User.findOne({email})
+    if(!user) {
+        throw new Error('Unable to login')
+    }
+    const isMatch = await bcrypt.compare(password,user.password)
+    if(!isMatch) {
+        throw new Error('Unable to login')
+    }
+
+    return user
+}
+
+/**
+ * Hash the plain text password before saving
+ */
+userSchema.pre('save', async function (next) {
+    const user = this
+
+    if(user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 8)
+    }
+
+    next()
+})
+
+/**
+ * Delete user tasks when the user is removed
+ */
+userSchema.pre('remove', async function (next) {
+    const user = this
+
+    await Task.deleteMany({owner: user._id})
+
+    next()
+})
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User
---
+++ src/models/incomingStudentsWishedCoursesForm.js
@@ -0,0 +1,66 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const Task = require('./task')
+
+const incomingStudentWishedCoursesFormSchema = new mongoose.Schema({
+    
+    ownerStudent: {
+        type: mongoose.SchemaType.Type.ObjectId,
+        required: true,
+        ref: 'IncomingStudent'
+    },
+    userType: {
+        type: Number,
+        default: 0,
+        validate(value) {
+            if(value < 0) {
+                throw new Error('User Type must be greater than zero')
+            }
+        }
+    },
+    wishedCourses: [{
+        course: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            ref: 'Course'
+        }
+    }]
+    
+    
+    
+}, {
+    timestamps: true
+})
+
+// // not stored in db for mongoose
+// userSchema.virtual('tasks', {
+//     ref: 'Task',
+//     localField: '_id',
+//     foreignField: 'owner'
+// })
+
+/*
+userSchema.virtual('university', {
+    ref: 'University',
+    localField: '_id',
+    foreignField: 'owner'
+})
+*/
+
+incomingStudentWishedCoursesFormSchema.methods.toJSON = function () {
+    const form = this
+    const formObject = form.toObject()
+
+    delete formObject.password
+    delete formObject.tokens
+    delete formObject.avatar
+
+    return formObject
+}
+
+
+const IncomingStudentsWishedCoursesForm = mongoose.model('IncomingStudentsWishedCoursesForm', incomingStudentWishedCoursesFormSchema)
+
+module.exports = IncomingStudentsWishedCoursesForm
