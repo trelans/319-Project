de96e6c2aa83297e511de7508f1221e284de88e5
İlker Özgen <110594591+ilkerozgenn@users.noreply.github.com>
2022-11-15 18:21:19 +0300

    ResetPasswordPage added to routing

--- erasmus-app/src/App.js
+++ erasmus-app/src/App.js
@@ -2,6 +2,7 @@ import LoginPage from "./pages/LoginPage/LoginPage";
 import MainPage from "./pages/MainPage/MainPage";
 import ForgotPasswordPage from "./pages/ForgotPasswordPage/ForgotPasswordPage";
 import ResetPasswordConfirmPage from "./pages/ResetPasswordConfirmPage/ResetPasswordConfirmPage";
+import ResetPasswordPage from "./pages/ResetPasswordPage/ResetPasswordPage";
 
 import { Route, Routes } from "react-router-dom";
 
@@ -19,6 +20,11 @@ function App() {
         exact
         path="/reset-password-confirm-page"
         element={<ResetPasswordConfirmPage />}
+      />
+      <Route
+        exact
+        path="/reset-password-page"
+        element={<ResetPasswordPage />}
       />
     </Routes>
   );
