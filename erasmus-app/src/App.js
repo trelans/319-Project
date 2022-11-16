9884296b0a9e82d99601566ed01da28b3f1891e7
trelans <59960883+trelans@users.noreply.github.com>
2022-11-16 16:11:46 +0300

    3243242
    4324234

--- erasmus-app/src/App.js
+++ erasmus-app/src/App.js
@@ -4,36 +4,42 @@ import ForgotPasswordPage from "./pages/ForgotPasswordPage/ForgotPasswordPage";
 import ResetPasswordConfirmPage from "./pages/ResetPasswordConfirmPage/ResetPasswordConfirmPage";
 import ResetPasswordPage from "./pages/ResetPasswordPage/ResetPasswordPage";
 import SettingsPage from "./pages/SettingsPage/SettingsPage";
-
-import { Route, Routes } from "react-router-dom";
+import ApplicantsList from "./pages/ApplicantsList/ApplicantsList"
+import {Route, Routes} from "react-router-dom";
 
 function App() {
-  return (
-    <Routes>
-      <Route exact path="/" element={<LoginPage />} />
-      <Route exact path="/main-page" element={<MainPage />} />
-      <Route
-        exact
-        path="/forgot-password-page"
-        element={<ForgotPasswordPage />}
-      />
-      <Route
-        exact
-        path="/reset-password-confirm-page"
-        element={<ResetPasswordConfirmPage />}
-      />
-      <Route
-        exact
-        path="/reset-password-page"
-        element={<ResetPasswordPage />}
-      />
-      <Route 
-      exact 
-      path="/settings-page"
-      element={<SettingsPage/>}
-      />
-    </Routes>
-  );
+    return (
+        <Routes>
+            <Route exact path="/" element={<LoginPage/>}/>
+            <Route exact path="/main-page" element={<MainPage/>}/>
+            <Route
+                exact
+                path="/forgot-password-page"
+                element={<ForgotPasswordPage/>}
+            />
+            <Route
+                exact
+                path="/reset-password-confirm-page"
+                element={<ResetPasswordConfirmPage/>}
+            />
+            <Route
+                exact
+                path="/reset-password-page"
+                element={<ResetPasswordPage/>}
+            />
+            <Route
+                exact
+                path="/settings-page"
+                element={<SettingsPage/>}
+            />
+            <Route
+                exact
+                path="/applicants-list"
+                element={<ApplicantsList/>}
+            />
+
+        </Routes>
+    );
 }
 
 export default App;
