a08cd6fcf8ecea2a60c583d4771002dbe6343672
Onurcan Ataç <onuratac7@gmail.com>
2022-11-20 21:25:35 +0300

    Back Button on Settings Page removed
    Back Button on Settings Page removed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 //import Routes from './pageRoutes'
-import React, {Component} from 'react'
-import { Routes, Route} from "react-router";
+import React, { Component } from "react";
+import { Routes, Route } from "react-router";
 
 import LoginPage from "./pages/LoginPage/LoginPage";
 import MainPage from "./pages/MainPage/MainPage";
@@ -8,7 +8,7 @@ import ForgotPasswordPage from "./pages/ForgotPasswordPage/ForgotPasswordPage";
 import ResetPasswordConfirmPage from "./pages/ResetPasswordConfirmPage/ResetPasswordConfirmPage";
 import ResetPasswordPage from "./pages/ResetPasswordPage/ResetPasswordPage";
 import SettingsPage from "./pages/SettingsPage/SettingsPage";
-import ApplicantsList from "./pages/ApplicantsList/ApplicantsList"
+import ApplicantsList from "./pages/ApplicantsList/ApplicantsList";
 
 /*
 class App extends Component {
@@ -57,41 +57,30 @@ class App extends Component {
 }
 */
 
-
 function App() {
-    return (
-        <Routes>
-            <Route exact path="/" element={<LoginPage/>}/>
-            <Route exact path="/main-page" element={<MainPage/>}/>
-            <Route
-                exact
-                path="/forgot-password-page"
-                element={<ForgotPasswordPage/>}
-            />
-            <Route
-                exact
-                path="/reset-password-confirm-page"
-                element={<ResetPasswordConfirmPage/>}
-            />
-            <Route
-                exact
-                path="/reset-password-page"
-                element={<ResetPasswordPage/>}
-            />
-            <Route
-                exact
-                path="/settings-page"
-                element={<SettingsPage/>}
-            />
-            <Route
-                exact
-                path="/applicants-list"
-                element={<ApplicantsList/>}
-            />
-
-        </Routes>
-    );
+  return (
+    <Routes>
+      <Route exact path="/" element={<LoginPage />} />
+      <Route exact path="/main-page" element={<MainPage />} />
+      <Route
+        exact
+        path="/forgot-password-page"
+        element={<ForgotPasswordPage />}
+      />
+      <Route
+        exact
+        path="/reset-password-confirm-page"
+        element={<ResetPasswordConfirmPage />}
+      />
+      <Route
+        exact
+        path="/reset-password-page"
+        element={<ResetPasswordPage />}
+      />
+      <Route exact path="/settings-page" element={<SettingsPage />} />
+      <Route exact path="/applicants-list" element={<ApplicantsList />} />
+    </Routes>
+  );
 }
-
 
 export default App;
