f19cf86522ce8f3f93f424cf8dff3dce7cb951cf
Ömer Oktay Gültekin <oktay.gultekin@ug.bilkent.edu.tr>
2022-11-22 17:13:56 +0300

    now user info can be seen in the console in dashboard

--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -1,6 +1,17 @@
-import { Link } from "react-router-dom";
+import {Link, useLocation} from "react-router-dom";
+import {handleRequests} from "../requests";
 
 function MainPage() {
+  const state = useLocation()
+  const id = state.id
+  handleRequests(state, {"id": id}, "dashboard", "1",(response, status) => {
+        // TODO Parameter state should be e normally, find how to send event without component from main page
+        console.log(response)
+      }
+  )
+  console.log(state)
+  console.log(id)
+  // Rerouting the user to the main page can be here if we do not see id or maybe can be doable using tokens
   return <div><div>Main Page</div><Link to="/settings-page"><button>Settings</button></Link></div>;
 }
 
