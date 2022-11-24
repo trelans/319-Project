9edbdf3ee32c0e0349b7587ad7df3a70a011758a
Onurcan Ataç <onuratac7@gmail.com>
2022-11-24 16:15:52 +0300

    Navbar to Settings Page
    Navbar to Settings Page

--- src/pages/SettingsPage/SettingsPage.js
+++ src/pages/SettingsPage/SettingsPage.js
@@ -1,35 +1,40 @@
 import { Link } from "react-router-dom";
+import NavigationBar from "../../components/ui/NavigationBar/NavigationBar";
 
 function SettingsPage() {
   return (
-    <div className="sp-container">
-      <form className="sp-center">
-        <div>
-          <h1 className="sp-h1">Settings</h1>
-        </div>
-        <div className="sp-btn-group">
-          <button className="sp-button">Change Password</button>
-          <button className="sp-button">Change E-Mail</button>
-        </div>
-        <div className="sp-center">
-          <label class="container" className="sp-label">
-            Disable Notifications
-            <input type="checkbox" className="sp-checkbox" />
-            <span class="checkmark"></span>
-          </label>
-        </div>
-        <div className="sp-center">
-          <label class="container" className="sp-label">
-            Send E-Mail for Notifications
-            <input
-              type="checkbox"
-              defaultChecked="true"
-              className="sp-checkbox"
-            />
-            <span class="checkmark"></span>
-          </label>
-        </div>
-      </form>
+    <div>
+      <NavigationBar />
+
+      <div className="sp-container">
+        <form className="sp-center">
+          <div>
+            <h1 className="sp-h1">Settings</h1>
+          </div>
+          <div className="sp-btn-group">
+            <button className="sp-button">Change Password</button>
+            <button className="sp-button">Change E-Mail</button>
+          </div>
+          <div className="sp-center">
+            <label class="container" className="sp-label">
+              Disable Notifications
+              <input type="checkbox" className="sp-checkbox" />
+              <span class="checkmark"></span>
+            </label>
+          </div>
+          <div className="sp-center">
+            <label class="container" className="sp-label">
+              Send E-Mail for Notifications
+              <input
+                type="checkbox"
+                defaultChecked="true"
+                className="sp-checkbox"
+              />
+              <span class="checkmark"></span>
+            </label>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
