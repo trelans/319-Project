425acc8bb8918b269547fd6cadc495e9194465ee
trelans <59960883+trelans@users.noreply.github.com>
2022-12-09 23:24:50 +0300

    Learning Agreement Before Mobility Part 3/3
    .

--- src/pages/LearningAgreement/LearningAgreementBeforeMobility3.js
+++ src/pages/LearningAgreement/LearningAgreementBeforeMobility3.js
@@ -2,57 +2,58 @@
 import { Link } from "react-router-dom";
 import NavigationBar from "../../components/ui/NavigationBar/NavigationBar";
 import InfoPageItem from "../InfoPage/InfoPageItem";
-import LABMStudentInfo from "../../components/ui/Tables/LearningAgreements/Stage1/StudentInfoForm"
-import LABMSendingInstution from "../../components/ui/Tables/LearningAgreements/Stage1/SendingInstutionForm"
-import LABMReceivingInstution from "../../components/ui/Tables/LearningAgreements/Stage1/ReceivingInstutionForm"
+import LABMStudent from "../../components/ui/Tables/LearningAgreements/Stage3/Student"
+import LABMSending from "../../components/ui/Tables/LearningAgreements/Stage3/ResponsiblePersonAtSendingIns"
+import LABMReceiving from "../../components/ui/Tables/LearningAgreements/Stage3/ResponsiblePersonAtReceivingIns"
 import Grid from "@material-ui/core/Grid";
 import React from "react";
 
-
-function LearningAgreementBeforeMobility1() {
+function LearningAgreementBeforeMobility3() {
     return(
         <div>
             <NavigationBar></NavigationBar>
 
             <div className="ip-container">
-                <h1 className="ip-h1">Learning Agreement Form Before Mobility Part 1 of 3:</h1>
+                <h1 className="ip-h1">Learning Agreement Form Before Mobility Part 3 of 3:</h1>
+
                 <Grid container   justifyContent="center" >
 
                     <Grid item xs={6} >
-                        <h1  style={{marginRight: 200, display: 'flex',  justifyContent:'center', alignItems:'center'}}>
-                            Student Info
-
+                        <h1 style={ { marginLeft : 150}} >
+                            Student
                         </h1>
-                        <p id="description" className="blackLetter" style={{marginRight: 200}}>
-                            Check your data
-                        </p>
-                        <LABMStudentInfo></LABMStudentInfo>
+
                     </Grid>
-
-
-                    <Grid item xs={4} >
-                        <h1 style={{marginLeft: 50, display: 'flex',  justifyContent:'center', alignItems:'center'}}>
-                            Sending Institution
+                    <Grid item xs={6} >
+                        <h1>
+                            Responsible Person at Sending Institution
                         </h1>
-                        <p id="description" className="blackLetter">
-                            Check your data
-                        </p>
-                        <LABMSendingInstution></LABMSendingInstution>
+
                     </Grid>
+                    <Grid item xs={6} >
 
+                        <LABMStudent></LABMStudent>
+                    </Grid>
+                    <Grid item xs={6} >
 
-
+                        <LABMSending></LABMSending>
+                    </Grid>
+                    <Grid item xs={6} >
+                        <h1 >
+                            Responsible Person at Receiving Institution
+                        </h1>
+                        <LABMReceiving></LABMReceiving>
+                    </Grid>
                     <Grid item xs={12} >
-                        <h1 id="title" className="blackLetter">
-                            Receiving Institution
-                        </h1>
-                        <p id="description" className="blackLetter">
-                            Check your data
-                        </p>
-                        <LABMReceivingInstution></LABMReceivingInstution>
+                      <div style={{ padding:30}}>
+                        <h6 >
+                            By signing this document, the student, the sending institution and the receiving institution confirm that they approve the proposed Learning Agreement and that they will comply with all the arrangements agreed by all parties. Sending and receiving institutions undertake to apply all the principles of the Erasmus Charter for Higher Education relating to mobility for studies (or the principles agreed in the inter-institutional agreement for institutions located in Partner Countries). The sending institution and the student should also commit to what is set out in the Erasmus+ grant agreement. The receiving institution confirms that the educational components listed in Table A are in line with its course catalogue and should be available to the student. The sending institution commits to recognise all the credits gained at the receiving institution for the successfully completed educational components and to count them towards the student's degree as described in Table C. Any exceptions to this rule are documented in an annex of this Learning Agreement and agreed by all parties. The student and receiving institution will communicate to the sending institution any problems or changes regarding the proposed mobility programme, responsible persons and/or study period.
+                        </h6>
+                      </div>
                     </Grid>
 
                 </Grid>
+
 
             </div>
 
@@ -60,4 +61,4 @@ function LearningAgreementBeforeMobility1() {
     );
 }
 
-export default LearningAgreementBeforeMobility1;
+export default LearningAgreementBeforeMobility3;
