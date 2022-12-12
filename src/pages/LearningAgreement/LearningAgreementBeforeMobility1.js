0d01db4672fc697e15b0f56c137261a83d4a9369
trelans <kutaysenyigit@hotmail.com>
2022-12-12 17:57:03 +0300

    Learning Agreement
    css eklendi buton eklendi

--- src/pages/LearningAgreement/LearningAgreementBeforeMobility1.js
+++ src/pages/LearningAgreement/LearningAgreementBeforeMobility1.js
@@ -7,7 +7,7 @@ import LABMSendingInstution from "../../components/ui/Tables/LearningAgreements/
 import LABMReceivingInstution from "../../components/ui/Tables/LearningAgreements/Stage1/ReceivingInstutionForm"
 import Grid from "@material-ui/core/Grid";
 import React from "react";
-
+import styles from "./LearningAgreementBeforeMobility.module.css"
 
 function LearningAgreementBeforeMobility1() {
     return(
@@ -16,45 +16,55 @@ function LearningAgreementBeforeMobility1() {
 
                 <div className="ip-container">
                     <h1 className="ip-h1">Learning Agreement Form Before Mobility Part 1 of 3:</h1>
-                    <Grid container   justifyContent="center" >
 
-                        <Grid item xs={6} >
-                            <h1  style={{marginRight: 200, display: 'flex',  justifyContent:'center', alignItems:'center'}}>
-                                Student Info
+                    <div className={styles.learningAgreement3}>
+                        <Grid container   justifyContent="center" >
 
-                            </h1>
-                            <p id="description" className="blackLetter" style={{marginRight: 200}}>
-                                Check your data
-                            </p>
-                            <LABMStudentInfo></LABMStudentInfo>
+                            <Grid item xs={6} >
+                                <h1  style={{marginRight: 200, display: 'flex',  justifyContent:'center', alignItems:'center'}}>
+                                    Student Info
+
+                                </h1>
+                                <p id="description" className="blackLetter" style={{marginRight: 200}}>
+                                    Check your data
+                                </p>
+                                <LABMStudentInfo></LABMStudentInfo>
+                            </Grid>
+
+
+                            <Grid item xs={4} >
+                                <h1 style={{marginLeft: 50, display: 'flex',  justifyContent:'center', alignItems:'center'}}>
+                                    Sending Institution
+                                </h1>
+                                <p id="description" className="blackLetter">
+                                    Check your data
+                                </p>
+                                <LABMSendingInstution></LABMSendingInstution>
+                            </Grid>
+
+
+
+                            <Grid item xs={12} >
+                                <h1 id="title" className="blackLetter">
+                                    Receiving Institution
+                                </h1>
+                                <p id="description" className="blackLetter">
+                                    Check your data
+                                </p>
+                                <LABMReceivingInstution></LABMReceivingInstution>
+                            </Grid>
+
                         </Grid>
-
-
-                        <Grid item xs={4} >
-                            <h1 style={{marginLeft: 50, display: 'flex',  justifyContent:'center', alignItems:'center'}}>
-                                Sending Institution
-                            </h1>
-                            <p id="description" className="blackLetter">
-                                Check your data
-                            </p>
-                            <LABMSendingInstution></LABMSendingInstution>
-                        </Grid>
-
-
-
-                        <Grid item xs={12} >
-                            <h1 id="title" className="blackLetter">
-                                Receiving Institution
-                            </h1>
-                            <p id="description" className="blackLetter">
-                                Check your data
-                            </p>
-                            <LABMReceivingInstution></LABMReceivingInstution>
-                        </Grid>
-
-                    </Grid>
-
+                    </div>
                 </div>
+
+            <div className="perfectCentered" >
+                <Link to="/learning-agreement-2-3">
+                    <button className = {styles.butConvert} >
+                        Next Step{" "}
+                    </button>
+                </Link>
+            </div>
 
         </div>
     );
--- src/pages/LearningAgreement/LearningAgreementBeforeMobility2.js
+++ src/pages/LearningAgreement/LearningAgreementBeforeMobility2.js
@@ -6,6 +6,7 @@ import Grid from "@material-ui/core/Grid";
 import React from "react";
 import RecognitionTable from "../../components/ui/Tables/LearningAgreements/Stage2/RecognitionAtSendingInst"
 import StudyProgrammeTable from "../../components/ui/Tables/LearningAgreements/Stage2/StudyProgrammeAtReceivingInst"
+import styles from "./LearningAgreementBeforeMobility.module.css"
 function LearningAgreementBeforeMobility2() {
     return(
         <div>
@@ -13,20 +14,29 @@ function LearningAgreementBeforeMobility2() {
 
             <div className="ip-container">
                 <h1 className="ip-h1">Learning Agreement Form Before Mobility Part 2 of 3:</h1>
-                <h1 style={{marginLeft: 50, display: 'flex',  justifyContent:'center', alignItems:'center'}}>
-                    Study Programme at Receiving Institution
-                </h1>
-                <p id="description" className="blackLetter">
-                    Planned period of the mobility: from [month/year] to [month/year]
-                </p>
-                <StudyProgrammeTable></StudyProgrammeTable>
+                <div className={styles.learningAgreement3}>
+                    <h1 style={{marginLeft: 50, display: 'flex',  justifyContent:'center', alignItems:'center'}}>
+                        Study Programme at Receiving Institution
+                    </h1>
+                    <p id="description" className="blackLetter">
+                        Planned period of the mobility: from [month/year] to [month/year]
+                    </p>
+                    <StudyProgrammeTable></StudyProgrammeTable>
 
-                <h1 style={{marginLeft: 50, display: 'flex',  justifyContent:'center', alignItems:'center'}}>
-                    Recognition at Sending Institution
-                </h1>
+                    <h1 style={{marginLeft: 50, display: 'flex',  justifyContent:'center', alignItems:'center'}}>
+                        Recognition at Sending Institution
+                    </h1>
 
-                <StudyProgrammeTable></StudyProgrammeTable>
+                    <StudyProgrammeTable></StudyProgrammeTable>
+                </div>
+            </div>
 
+            <div className="perfectCentered">
+                <Link to="/learning-agreement-3-3">
+                    <button className = {styles.butConvert} >
+                        Next Step{" "}
+                    </button>
+                </Link>
             </div>
 
         </div>
