2f4db67c52977903c45cbad5291b56e303360c40
trelans <59960883+trelans@users.noreply.github.com>
2022-12-14 02:41:36 +0300

    Learning Agreement After Mobility

---
+++ src/pages/LearningAgreement/LearningAgreementAfterMobility.js
@@ -0,0 +1,48 @@
+
+import { Link } from "react-router-dom";
+import NavigationBar from "../../components/ui/NavigationBar/NavigationBar";
+
+import Grid from "@mui/material/Grid";
+import React from "react";
+
+import Sending from "../../components/ui/Tables/LearningAgreements/StageA/Sending"
+import Receiving from "../../components/ui/Tables/LearningAgreements/StageA/Recieving"
+
+import styles from "./LearningAgreementBeforeMobility.module.css"
+function LearningAgreementBeforeMobility2() {
+    return(
+        <div>
+            <NavigationBar></NavigationBar>
+
+            <div className="ip-container">
+                <h1 className="ip-h1">Learning Agreement Form After Mobility</h1>
+                <div className={styles.learningAgreement3}>
+                    <h1 style={{marginLeft: 50, display: 'flex',  justifyContent:'center', alignItems:'center'}}>
+                        Study Programme at Receiving Institution
+                    </h1>
+                    <p id="description" className="blackLetter">
+                        Planned period of the mobility: from [month/year] to [month/year]
+                    </p>
+                    <Receiving></Receiving>
+
+                    <h1 style={{marginLeft: 50, display: 'flex',  justifyContent:'center', alignItems:'center'}}>
+                        Recognition at Sending Institution
+                    </h1>
+
+                        <Sending></Sending>
+                </div>
+            </div>
+
+            <div className="perfectCentered">
+
+                    <button className = {styles.butConvert} >
+                        Convert To Pdf{" "}
+                    </button>
+
+            </div>
+
+        </div>
+    );
+}
+
+export default LearningAgreementBeforeMobility2;
