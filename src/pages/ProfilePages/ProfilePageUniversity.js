f15236bf64568399648e27f71eede10c99aff060
İlker Özgen <110594591+ilkerozgenn@users.noreply.github.com>
2022-12-16 14:41:43 +0300

    Profile University page patch

--- src/pages/ProfilePages/ProfilePageUniversity.js
+++ src/pages/ProfilePages/ProfilePageUniversity.js
@@ -1,54 +1,52 @@
 import NavigationBar from "../../components/ui/NavigationBar/NavigationBar";
 import img from "./image.png";
-import {useRef, useState} from "react";
-import {handleRequests} from "../requests";
+import { useRef, useState } from "react";
+import { handleRequests } from "../requests";
 import * as React from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 let loaded = false;
 
-
 function ProfilePageUniversity() {
+  const [name, setName] = useState("");
+  const [country, setCountry] = useState("");
+  const [websiteLink, setWebsiteLink] = useState("");
+  const [feedbacks, setFeedbacks] = useState([]);
+  const [mobilityPeriod, setMobilityPeriod] = useState("");
+  const [erasmusCode, setErasmusCode] = useState("");
 
+  const [isLoading, setLoading] = React.useState(true);
 
+  if (!loaded) {
+    handleRequests(
+      null,
+      { name: "Roskilde University" },
+      "profile-university",
+      "1",
+      (response, status) => {
+        setName(response.name);
+        setCountry(response.country);
+        setWebsiteLink(response.websiteLink);
+        setFeedbacks(response.feedbacks);
+        setErasmusCode(response.erasmusCode);
+        setMobilityPeriod(response.mobilityPeriod);
 
-  const [name, setName] = useState('')
-  const [country, setCountry] = useState('')
-  const [websiteLink, setWebsiteLink] = useState('')
-  const [feedbacks, setFeedbacks] = useState([])
-  const [mobilityPeriod, setMobilityPeriod] = useState('')
-  const [erasmusCode, setErasmusCode] = useState('')
-
-  const [isLoading, setLoading] = React.useState(true)
-
-
-
-  if(!loaded) {
-    handleRequests(null, {"name": "Roskilde University"}, "profile-university", "1", (response, status) => {
-
-      setName(response.name)
-      setCountry(response.country)
-      setWebsiteLink(response.websiteLink)
-      setFeedbacks(response.feedbacks)
-      setErasmusCode(response.erasmusCode)
-      setMobilityPeriod((response.mobilityPeriod))
-
-      console.log(name)
-      console.log(country)
-
-
-    })
-    loaded = true
-    setLoading(false)
+        console.log(name);
+        console.log(country);
+      }
+    );
+    loaded = true;
+    setLoading(false);
   }
 
   if (isLoading) {
-    return <div className={"Page"}>
-      <NavigationBar/>
-      <div className="App">Loading...</div>
-    </div>;
+    return (
+      <div className={"Page"}>
+        <NavigationBar />
+        <div className="App">Loading...</div>
+      </div>
+    );
   }
-
 
   return (
     <div>
@@ -87,69 +85,63 @@ function ProfilePageUniversity() {
                 <br />
               </td>
             </tr>
-            <tr>
-              <td>
-                <br />
-              </td>
-            </tr>
-            <tr>
-              <td className="ppu-text-other">
-                <a
-                  className="ppu-link"
-                  href="https://w3.bilkent.edu.tr/www/"
-                  target="_blank"
-                >
-                  Official Website
-                </a>
-              </td>
-            </tr>
           </table>
+          <tr>
+            <td className="ppu-text-other"></td>
+          </tr>
         </div>
+        <a
+          className="ppu-link"
+          href="https://w3.bilkent.edu.tr/www/"
+          target="_blank"
+        >
+          Official Website
+        </a>
       </div>
       <div className="ppu-table-container">
-      <table className="ppu-table2">
-        <tr>
-          <td className="ppu-td">
-            <p className="ppu-table-header">Comments</p>
-          </td>
-          <td className="ppu-td">
-            <p className="ppu-table-header">Rate</p>
-          </td>
-        </tr>
-        <tr>
-          <td className="ppu-td">
-            <p className="ppu-table-other">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            </p>
-          </td>
-          <td className="ppu-td">
-            <p className="ppu-table-other">4/5</p>
-          </td>
-        </tr>
-        <tr>
-          <td className="ppu-td">
-            <p className="ppu-table-other">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            </p>
-          </td>
-          <td className="ppu-td">
-            <p className="ppu-table-other">3/5</p>
-          </td>
-        </tr>
-        <tr>
-          <td className="ppu-td">
-            <p className="ppu-table-other">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            </p>
-          </td>
-          <td className="ppu-td">
-            <p className="ppu-table-other">3.5/5</p>
-          </td>
-        </tr>
-      </table>
+        <table className="ppu-table2">
+          <tr>
+            <td className="ppu-td">
+              <p className="ppu-table-header">Comments</p>
+            </td>
+            <td className="ppu-td">
+              <p className="ppu-table-header">Rate</p>
+            </td>
+          </tr>
+          <tr>
+            <td className="ppu-td">
+              <p className="ppu-table-other">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+            </td>
+            <td className="ppu-td">
+              <p className="ppu-table-other">4/5</p>
+            </td>
+          </tr>
+          <tr>
+            <td className="ppu-td">
+              <p className="ppu-table-other">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+            </td>
+            <td className="ppu-td">
+              <p className="ppu-table-other">3/5</p>
+            </td>
+          </tr>
+          <tr>
+            <td className="ppu-td">
+              <p className="ppu-table-other">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+            </td>
+            <td className="ppu-td">
+              <p className="ppu-table-other">3.5/5</p>
+            </td>
+          </tr>
+        </table>
       </div>
     </div>
   );
