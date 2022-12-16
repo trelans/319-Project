44b2af38ab233cd70d15e0f7f8511d1390fc92a0
İlker Özgen <110594591+ilkerozgenn@users.noreply.github.com>
2022-12-16 14:06:43 +0300

    Pdf Converter is working

--- src/pages/ConvertToPdfPages/PreApprovalFormConvert.js
+++ src/pages/ConvertToPdfPages/PreApprovalFormConvert.js
@@ -2,6 +2,7 @@ import { handleRequests } from "../requests";
 import { useState } from "react";
 import NavigationBar from "../../components/ui/NavigationBar/NavigationBar";
 import * as React from "react";
+import Pdf from "react-to-pdf";
 
 const durationTable = {
   0: "Fall",
@@ -11,12 +12,16 @@ const durationTable = {
 
 let loaded = false;
 
+const ref = React.createRef();
+
 function PreApprovalFormConvert() {
   const [candName, setCandName] = useState("");
   const [candSurname, setCandSurname] = useState("");
   const [candID, setCandID] = useState("");
   const [candDepartment, setCandDepartment] = useState("");
   const [hostUniName, setHostUniName] = useState("");
+  const [academicYear, setAcademicYear] = useState("");
+  const [semester, setSemester] = useState("");
   const [duration, setDuration] = useState("");
   const [ECTSCredits, setECTSCredits] = useState("");
   const [courses, setCourses] = useState([]);
@@ -31,13 +36,15 @@ function PreApprovalFormConvert() {
       setCandID(response.id);
       setCandDepartment(response.department);
       setHostUniName(response.appliedInstitution);
+      setAcademicYear(response.academicYear);
+      setSemester(response.semester);
       setDuration(durationTable[response.duration]);
       setECTSCredits(response.ECTSCredits);
       setCourses(response.courses);
       setBilkentCourses(response.bilkentCourses);
     });
     loaded = true;
-    setLoading(false);
+    setLoading(false);  
   }
 
   if (isLoading) {
@@ -50,17 +57,67 @@ function PreApprovalFormConvert() {
 
   return (
     <div>
-      <div>
-        <table className="cp-table">
-          <tr className="cp-row">
-            <td className="cp-col">Name:</td>
-            <td className="cp-col">{candName}</td>
-          </tr>
-          <tr className="cp-row">
-            <td className="cp-col">Surname:</td>
-            <td className="cp-col">asgsadgf</td>
-          </tr>
-        </table>
+      <div className="cp-center">
+        <Pdf targetRef={ref} filename="PreApprovalForm.pdf">
+          {({ toPdf }) => (
+            <button className="cp-button" onClick={toPdf}>
+              Download PDF
+            </button>
+          )}
+        </Pdf>
+      </div>
+      <div ref={ref}>
+        <div className="cp-container">
+          <div className="cp-center">
+            <h1 className="cp-h1">
+              Course Exemption Pre-Approval Form for Outgoing Students
+            </h1>
+          </div>
+        </div>
+        <div className="cp-container">
+          <div className="cp-center">
+            <table className="cp-table">
+              <tr className="cp-row">
+                <td className="cp-col">Name:</td>
+                <td className="cp-col">{candName}</td>
+              </tr>
+              <tr className="cp-row">
+                <td className="cp-col">Surname:</td>
+                <td className="cp-col">{candSurname}</td>
+              </tr>
+              <tr className="cp-row">
+                <td className="cp-col">ID Number:</td>
+                <td className="cp-col">{candID}</td>
+              </tr>
+              <tr className="cp-row">
+                <td className="cp-col">Department:</td>
+                <td className="cp-col">{candDepartment}</td>
+              </tr>
+            </table>
+          </div>
+        </div>
+        <div className="cp-container">
+          <div className="cp-center">
+            <table className="cp-table">
+              <tr className="cp-row">
+                <td className="cp-col">Name of the host institution:</td>
+                <td className="cp-col">{hostUniName}</td>
+              </tr>
+              <tr className="cp-row">
+                <td className="cp-col">Academic Year:</td>
+                <td className="cp-col">{academicYear}</td>
+              </tr>
+              <tr className="cp-row">
+                <td className="cp-col">Semester:</td>
+                <td className="cp-col">{semester}</td>
+              </tr>
+              <tr className="cp-row">
+                <td className="cp-col">Duration:</td>
+                <td className="cp-col">{duration}</td>
+              </tr>
+            </table>
+          </div>
+        </div>
       </div>
     </div>
   );
