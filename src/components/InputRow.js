40eb008919135f0a12a6479d8853cba97a0a240e
İlker Özgen <110594591+ilkerozgenn@users.noreply.github.com>
2022-12-16 23:41:38 +0300

    InputRow patch

--- src/components/InputRow.js
+++ src/components/InputRow.js
@@ -2,12 +2,7 @@ import { IconButton, Stack, TextField } from "@mui/material";
 import RemoveIcon from "@mui/icons-material/Remove";
 import AddIcon from "@mui/icons-material/Add";
 
-const arr = [
-  { value: "", text: "--Choose an option--" },
-  { value: "apple", text: "Apple 🍏" },
-  { value: "banana", text: "Banana 🍌" },
-  { value: "kiwi", text: "Kiwi 🥝" },
-];
+
 
 export const InputRow = ({
   index,
@@ -19,65 +14,57 @@ export const InputRow = ({
   inputFields,
 }) => {
   return (
-    <Stack spacing={2} style={{ alignItems: "center" }}>
-      <TextField
-        name="courseCode"
-        required
-        fullWidth
-        label="Course Code"
-        onChange={(event) => handleChange(event, index)}
-        value={item.courseCode}
-      />
-      <TextField
-        name="courseName"
-        required
-        fullWidth
-        label="Course Name"
-        onChange={(event) => handleChange(event, index)}
-        value={item.courseName}
-      />
+    <div>
+      <Stack spacing={2} style={{ alignItems: "center" }}>
+        <TextField
+          name="courseCode"
+          required
+          fullWidth
+          label="Course Code"
+          onChange={(event) => handleChange(event, index)}
+          value={item.courseCode}
+        />
+        <TextField
+          name="courseName"
+          required
+          fullWidth
+          label="Course Name"
+          onChange={(event) => handleChange(event, index)}
+          value={item.courseName}
+        />
 
-      <TextField
-        name="credits"
-        fullWidth
-        label="ECTS Credits"
-        onChange={(event) => handleChange(event, index)}
-        value={item.credits}
-      />
-      <TextField
-        name="website"
-        multiline
-        fullWidth
-        label="Website Link"
-        onChange={(event) => handleChange(event, index)}
-        value={item.website}
-      />
-      <TextField
-        name="syllabus"
-        fullWidth
-        label="Syllabus Link"
-        onChange={(event) => handleChange(event, index)}
-        value={item.syllabusLink}
-      />
+        <TextField
+          name="credits"
+          fullWidth
+          label="ECTS Credits"
+          onChange={(event) => handleChange(event, index)}
+          value={item.credits}
+        />
+        <TextField
+          name="website"
+          multiline
+          fullWidth
+          label="Website Link"
+          onChange={(event) => handleChange(event, index)}
+          value={item.website}
+        />
+        <TextField
+          name="syllabus"
+          fullWidth
+          label="Syllabus Link"
+          onChange={(event) => handleChange(event, index)}
+          value={item.syllabusLink}
+        />
 
-      <div>
         <div>
-          <select onChange={handleChange} name="fruits" id="fruit-select">
-            {arr.map((option, index) => (
-              <option key={index} value={option.value}>
-                {option.text}
-              </option>
-            ))}
-          </select>
+          <IconButton onClick={handleRemove}>
+            <RemoveIcon />
+          </IconButton>
+          <IconButton onClick={handleAdd}>
+            <AddIcon />
+          </IconButton>
         </div>
-
-        <IconButton onClick={handleRemove}>
-          <RemoveIcon />
-        </IconButton>
-        <IconButton onClick={handleAdd}>
-          <AddIcon />
-        </IconButton>
-      </div>
-    </Stack>
+      </Stack>
+    </div>
   );
 };
