8140d2938aee42e567d238bac17f53832cbf3141
Utku Kurtulmuş <utkukurtulmus@hotmail.com>
2022-12-18 16:15:05 +0300

    LogsPage

--- src/pages/LogsPage/LogsPage.js
+++ src/pages/LogsPage/LogsPage.js
@@ -1,5 +1,31 @@
-function LogsPage() {
-  return <div>Logs Page</div>;
+import axios from "axios";
+
+async function LogsPage() {
+
+  var notifications;
+
+  async function getLogs() {
+
+    const res = await axios.get(`http://localhost:8080/notifications`, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    });
+
+    notifications = res.data
+    console.log(notifications)
+  }
+
+  await getLogs();
+
+  return <div>
+      <div> LOGS </div>
+      {notifications?.map( (item) => {
+        return (
+          <div>{item.text}</div>
+        )
+      })}
+  </div>;
 }
 
 export default LogsPage;
