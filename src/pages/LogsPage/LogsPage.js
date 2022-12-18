0275abeff5464bb18c8c17da735e46be3a566df5
trelans <59960883+trelans@users.noreply.github.com>
2022-12-18 16:14:43 +0300

    logs Page
    dsfds

--- src/pages/LogsPage/LogsPage.js
+++ src/pages/LogsPage/LogsPage.js
@@ -1,5 +1,10 @@
+import LogsTable from "../../components/ui/CoursePopUp/Tables/LogsTable"
+import NavigationBar from "../../components/ui/NavigationBar/NavigationBar";
+
 function LogsPage() {
-  return <div>Logs Page</div>;
+  return <div><NavigationBar/>
+    <div style={{marginTop: 10}}>
+    <LogsTable></LogsTable></div></div>;
 }
 
 export default LogsPage;
