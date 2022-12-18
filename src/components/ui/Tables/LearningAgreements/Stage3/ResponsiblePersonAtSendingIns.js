ad30e457854d67ea99945bf871a4a132c14cb090
mertunlu <69141853+mertunlu@users.noreply.github.com>
2022-12-18 04:49:48 +0300

    pre-approval form page 3-3 old version kutay için.

--- src/components/ui/Tables/LearningAgreements/Stage3/ResponsiblePersonAtSendingIns.js
+++ src/components/ui/Tables/LearningAgreements/Stage3/ResponsiblePersonAtSendingIns.js
@@ -10,45 +10,40 @@ import "react-phone-input-2/lib/style.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { MenuItem } from "@mui/material";
 import Grid from "@mui/material/Grid";
-import { handleRequests } from "../../../../../pages/requests";
 
 const options = [
   { value: "female", label: "Female" },
   { value: "male", label: "Male" },
   { value: "other", label: "Other" },
 ];
-export default class App extends React.Component {
+export default class Student extends React.Component {
   constructor(props) {
     super(props);
     this.options = countryList().getData();
-    console.log(props);
     this.state = {
       startDate: new Date(),
+      country: "",
       options: this.options,
-
+      countryVal: 2,
+      complete: "",
       displayComplete: "none",
-
-      id: props.id,
-
-      name: props.fields ? props.fields.name : "",
-      personFunction: props.fields ? props.fields.personFunction : "",
-      phoneNumber: props.fields ? props.fields.phoneNumber : "",
-      email: props.fields ? props.fields.email : "",
-      signature: props.fields ? props.fields.signature : "",
-      //date almamışsınız
+      name: "Kingston University",
+      function: "Engineering",
+      academicYear: "2022",
+      studyCycle: "Onurcan",
+      address: "290 Street LA 2032  ",
+      subjectAreaCode: "onurcanatac@bilkent.edu.tr",
+      email: "omer.oktay.gultekin@erasmusapp.com",
 
       disabledName: true,
-      disabledPersonFunction: true,
-      disabledPhoneNumber: true,
-      disabledEmail: true,
-      disabledSignature: true,
+      disabledLastName: true,
+      disabledDateOfBirth: true,
+      disabledNationality: true,
+      disabledGender: true,
+      disabledAcademicYear: true,
+      disabledStudyCycle: true,
+      disabledSubjectArea: true,
     };
-
-    this.updateInputName = this.updateInputName.bind(this);
-    this.updateInputPersonFunction = this.updateInputPersonFunction.bind(this);
-    this.updateInputPhoneNumber = this.updateInputPhoneNumber.bind(this);
-    this.updateInputEmail = this.updateInputEmail.bind(this);
-    this.updateInputSignature = this.updateInputSignature.bind(this);
 
     this.flagsRef = React.createRef();
     this.lastnameRef = React.createRef();
@@ -59,22 +54,6 @@ export default class App extends React.Component {
     this.studyCycleRef = React.createRef();
     this.subjectAreaCodeRef = React.createRef();
     this.handleChangeDate = this.handleChangeDate.bind(this);
-  }
-
-  updateInputName(event) {
-    this.setState({ name: event.target.value });
-  }
-  updateInputPersonFunction(event) {
-    this.setState({ personFunction: event.target.value });
-  }
-  updateInputPhoneNumber(event) {
-    this.setState({ phoneNumber: event.target.value });
-  }
-  updateInputEmail(event) {
-    this.setState({ email: event.target.value });
-  }
-  updateInputSignature(event) {
-    this.setState({ signature: event.target.value });
   }
 
   handleChangeGender = (gender) => {
@@ -94,183 +73,266 @@ export default class App extends React.Component {
     this.setState({ disabledName: !this.state.disabledName });
   }
 
-  handleEditPersonFunctionClick() {
-    this.setState({
-      disabledPersonFunction: !this.state.disabledPersonFunction,
-    });
+  handleEditLastNameClick() {
+    this.setState({ disabledLastName: !this.state.disabledLastName });
+  }
+  handleEditDateOfBirthClick() {
+    this.setState({ disabledDateOfBirth: !this.state.disabledDateOfBirth });
+  }
+  handleEditNationalityClick() {
+    this.setState({ disabledNationality: !this.state.disabledNationality });
+  }
+  handleEditGenderClick() {
+    this.setState({ disabledGender: !this.state.disabledGender });
+  }
+  handleEditAcademicYearClick() {
+    this.setState({ disabledAcademicYear: !this.state.disabledAcademicYear });
+  }
+  handleEditStudyCycleClick() {
+    this.setState({ disabledStudyCycle: !this.state.disabledStudyCycle });
+  }
+  handleSubjectAreaClick() {
+    this.setState({ disabledSubjectArea: !this.state.disabledSubjectArea });
   }
 
-  handleEditPhoneNumberClick() {
-    this.setState({ disabledPhoneNumber: !this.state.disabledPhoneNumber });
+  handlerComplete(e) {
+    if (
+        this.state.countryVal !== null &&
+        this.state.complete === true &&
+        this.state.phone !== ""
+    ) {
+      this.setState({
+        displayComplete: "flex",
+      });
+      window.scroll({
+        top: this.complete.offsetTop,
+        left: 0,
+        behavior: "smooth",
+      });
+    } else {
+      alert("You have not yet completed the form");
+    }
   }
-
-  handleEditEmailClick() {
-    this.setState({ disabledEmail: !this.state.disabledEmail });
-  }
-
-  handleEditSignatureClick() {
-    this.setState({ disabledSignature: !this.state.disabledSignature });
-  }
-
-  handlerComplete = (e) => {
-    const responsiblePersonFromSendingInsInfo = {
-      id: this.state.id,
-      infoType: 1,
-      responsiblePersonFromSendingIns: {
-        name: this.state.name,
-        personFunction: this.state.personFunction,
-        email: this.state.email,
-        signature: this.state.signature,
-        phoneNumber: this.state.phoneNumber,
-      },
-    };
-
-    handleRequests(
-      e,
-      responsiblePersonFromSendingInsInfo,
-      "learning-agreement-3-3",
-      "2",
-      (response, status) => {
-        if (response.status === 200) {
-          this.savedInfo = (
-            <div>
-              <p>Fields are saved!</p>
-            </div>
-          );
-        }
-      }
-    );
-  };
 
   render() {
     return (
-      <div className={"App"}>
-        <div>
-          <div id="survey-form">
-            <Grid container spacing={2}>
-              <Grid item xs={4}>
-                <label className={"textHeader"} id="name-label" htmlFor="name">
-                  Name
-                </label>
+        <div className={"App"}>
+          <div>
+            <div id="survey-form">
+              <Grid container spacing={2}>
+                <Grid item xs={4}>
+                  <label className={"textHeader"} id="name-label" htmlFor="name">
+                    Name
+                  </label>
+                </Grid>
+
+                <Grid item xs={6}>
+                  <div id="nameCircle" ref={(div) => (this.nameCircle = div)} />
+                  <input
+                      ref={"this.nameRef"}
+                      id="name"
+                      maxLength="30"
+                      placeholder="write your name"
+                      pattern="[A-Za-z]"
+                      className="styleInput"
+                      defaultValue={this.state.name}
+                      disabled={this.state.disabledName ? "disabledName" : ""}
+                      required
+                  />
+                </Grid>
+
+                <Grid item xs={1}>
+                  <button
+                      className="editButton"
+                      onClick={this.handleEditNameClick.bind(this)}
+                      disabled={!this.state.disabledName}
+                  >
+                    {" "}
+                    Edit
+                  </button>
+                </Grid>
+
+                <Grid item xs={4}>
+                  <label
+                      className={"textHeader"}
+                      id="lastName-label"
+                      htmlFor="Last name"
+                  >
+                    Function
+                  </label>
+                </Grid>
+
+                <div />
+
+                <Grid item xs={6}>
+                  <input
+                      ref={"this.lastnameRef"}
+                      id="function"
+                      maxLength="30"
+                      pattern="[A-Za-z]"
+                      defaultValue={this.state.function}
+                      className="styleInput"
+                      disabled={
+                        this.state.disabledLastName ? "disabledLastName" : ""
+                      }
+                      required
+                  />
+                </Grid>
+
+                <Grid item xs={1}>
+                  <button
+                      className="editButton"
+                      onClick={this.handleEditLastNameClick.bind(this)}
+                      disabled={!this.state.disabledLastName}
+                  >
+                    {" "}
+                    Edit
+                  </button>
+                </Grid>
+
+                <Grid item xs={4}>
+                  <label
+                      className={"textHeader"}
+                      id="lastName-label"
+                      htmlFor="Address"
+                  >
+                    Phone Number
+                  </label>
+                </Grid>
+
+                <Grid item xs={6}>
+                  <PhoneInput
+                      country={"us"}
+                      className="marginBottom"
+                      value={this.state.phone}
+                      onChange={(phone) => this.setState({ phone })}
+                  />
+                </Grid>
+
+                <Grid item xs={1}>
+                  <button
+                      className="editButton "
+                      onClick={this.handleSubjectAreaClick.bind(this)}
+                      disabled={!this.state.disabledSubjectArea}
+                  >
+                    {" "}
+                    Edit
+                  </button>
+                </Grid>
+
+                <Grid item xs={4}>
+                  <label
+                      className={"textHeader"}
+                      id="Dateofbirth-label"
+                      htmlFor="Date of Birth"
+                  >
+                    Mail
+                  </label>
+                </Grid>
+
+                <Grid item xs={6}>
+                  <input
+                      ref={"this.mail"}
+                      id="mail"
+                      maxLength="30"
+                      defaultValue={this.state.email}
+                      className="styleInput"
+                      disabled={this.state.disabledGender ? "disabledGender" : ""}
+                      required
+                  />
+                </Grid>
+
+                <Grid item xs={1}>
+                  <button
+                      className="editButton "
+                      onClick={this.handleEditGenderClick.bind(this)}
+                      disabled={!this.state.disabledGender}
+                  >
+                    {" "}
+                    Edit
+                  </button>
+                </Grid>
+
+                <Grid item xs={4}>
+                  <label
+                      className={"textHeader"}
+                      id="Dateofbirth-label"
+                      htmlFor="Date of Birth"
+                  >
+                    Date
+                  </label>
+                </Grid>
+
+                <Grid item xs={6}>
+                  <DatePicker
+                      ref={"this.dateOfBirthRef"}
+                      selected={this.state.startDate}
+                      onChange={this.handleChangeDate}
+                      className="styleInput"
+                      disabled={
+                        this.state.disabledDateOfBirth ? "disabledDateOfBirth" : ""
+                      }
+                  />
+                </Grid>
+
+                <Grid item xs={1}>
+                  <button
+                      className="editButton"
+                      onClick={this.handleEditDateOfBirthClick.bind(this)}
+                      disabled={!this.state.disabledDateOfBirth}
+                  >
+                    {" "}
+                    Edit
+                  </button>
+                </Grid>
+
+                <Grid item xs={4}>
+                  <label
+                      className={"textHeader"}
+                      id="lastName-label"
+                      htmlFor="country"
+                  >
+                    Signature
+                  </label>
+                </Grid>
+
+                <Grid item xs={6}>
+                  <input
+                      ref={"this.academicYear"}
+                      id="name"
+                      maxLength="30"
+                      className="styleInput"
+                      defaultValue={this.state.academicYear}
+                      disabled={
+                        this.state.disabledAcademicYear
+                            ? "disabledAcademicYear"
+                            : ""
+                      }
+                      required
+                  />
+                </Grid>
+
+                <Grid item xs={1}>
+                  <button
+                      className="editButton "
+                      onClick={this.handleEditAcademicYearClick.bind(this)}
+                      disabled={!this.state.disabledAcademicYear}
+                  >
+                    {" "}
+                    Edit
+                  </button>
+                </Grid>
+
+                <Grid item xs={12}></Grid>
               </Grid>
-
-              <Grid item xs={6}>
-                <div id="nameCircle" ref={(div) => (this.nameCircle = div)} />
-                <input
-                  ref={"this.nameRef"}
-                  id="name"
-                  maxLength="30"
-                  placeholder="write your name"
-                  pattern="[A-Za-z]"
-                  className="styleInput"
-                  defaultValue={this.state.name}
-                  disabled={this.state.disabledName ? "disabledName" : ""}
-                  required
-                />
-              </Grid>
-
-              <Grid item xs={4}>
-                <label
-                  className={"textHeader"}
-                  id="lastName-label"
-                  htmlFor="Last name"
-                >
-                  Function
-                </label>
-              </Grid>
-
-              <div />
-
-              <Grid item xs={6}>
-                <input
-                  ref={"this.lastnameRef"}
-                  id="function"
-                  maxLength="30"
-                  pattern="[A-Za-z]"
-                  defaultValue={this.state.personFunction}
-                  className="styleInput"
-                  disabled={
-                    this.state.disabledPersonFunction
-                      ? "disabledPersonFunction"
-                      : ""
-                  }
-                  required
-                />
-              </Grid>
-
-              <Grid item xs={4}>
-                <label
-                  className={"textHeader"}
-                  id="lastName-label"
-                  htmlFor="Address"
-                >
-                  Phone Number
-                </label>
-              </Grid>
-
-              <Grid item xs={6}>
-                <PhoneInput
-                  country={"us"}
-                  className="marginBottom"
-                  value={this.state.phoneNumber}
-                  onChange={(phoneNumber) => this.setState({ phoneNumber })}
-                />
-              </Grid>
-
-              <Grid item xs={4}>
-                <label
-                  className={"textHeader"}
-                  id="Dateofbirth-label"
-                  htmlFor="Date of Birth"
-                >
-                  Mail
-                </label>
-              </Grid>
-
-              <Grid item xs={6}>
-                <input
-                  ref={"this.mail"}
-                  id="mail"
-                  maxLength="30"
-                  defaultValue={this.state.email}
-                  className="styleInput"
-                  disabled={this.state.disabledEmail ? "disabledEmail" : ""}
-                  required
-                />
-              </Grid>
-
-              <Grid item xs={4}>
-                <label
-                  className={"textHeader"}
-                  id="Dateofbirth-label"
-                  htmlFor="Date of Birth"
-                >
-                  Date
-                </label>
-              </Grid>
-
-              <Grid item xs={6}>
-                <DatePicker
-                  ref={"this.dateOfBirthRef"}
-                  selected={this.state.startDate}
-                  onChange={this.handleChangeDate}
-                  className="styleInput"
-                  disabled={this.state.disabledName ? "disabledName" : ""}
-                />
-              </Grid>
-
-
-
-              <Grid item xs={12}></Grid>
-            </Grid>
-            <div className="perfectCentered">
-              <button className="buttons" onClick={this.handlerComplete}>
-                Save{" "}
-              </button>
+              <div className="perfectCentered">
+                <button className="buttons" onClick={this.handlerComplete}>
+                  Save{" "}
+                </button>
+              </div>
             </div>
           </div>
         </div>
-      </div>
     );
   }
 }
