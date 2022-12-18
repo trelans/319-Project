a332f88f8a13a1a9b21b64121bfdfef799259978
Onurcan Ataç <onuratac7@gmail.com>
2022-12-18 03:39:57 +0300

    NominationRequestTable Update

--- src/components/ui/CoursePopUp/Tables/NomRequestTable.js
+++ src/components/ui/CoursePopUp/Tables/NomRequestTable.js
@@ -9,83 +9,104 @@ import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
 import SearchBar from "material-ui-search-bar";
 
-interface food {
-    name: string;
-    calories: number;
-    fat: number;
-    carbs: number;
-    protein: number;
+interface waitingCourse {
+  requestNo: string;
+  courseCode: number;
+  courseName: number;
 }
 
 const useStyles = makeStyles({
-    table: {
-        minWidth: 650
-    }
+  table: {
+    minWidth: 650,
+  },
 });
 
-const originalRows: food[] = [
-    { name: "Pizza", calories: 200, fat: 6.0, carbs: 24, protein: 4.0 },
-    { name: "Hot Dog", calories: 300, fat: 6.0, carbs: 24, protein: 4.0 },
-    { name: "Burger", calories: 400, fat: 6.0, carbs: 24, protein: 4.0 },
-    { name: "Hamburger", calories: 500, fat: 6.0, carbs: 24, protein: 4.0 },
-    { name: "Fries", calories: 600, fat: 6.0, carbs: 24, protein: 4.0 },
-    { name: "Ice Cream", calories: 700, fat: 6.0, carbs: 24, protein: 4.0 }
+const originalRows: waitingCourse[] = [
+  {
+    request: "1",
+    courseCode: "CS 331",
+    courseName: "Fundamental Fundamental Fundementaling",
+  },
+  {
+    request: "2",
+    courseCode: "CS 331",
+    courseName: "Fundamental Fundamental Fundementaling",
+  },
+  {
+    request: "3",
+    courseCode: "CS 331",
+    courseName: "Fundamental Fundamental Fundementaling",
+  },
+  {
+    request: "4",
+    courseCode: "CS 331",
+    courseName: "Fundamental Fundamental Fundementaling",
+  },
+  {
+    request: "5",
+    courseCode: "CS 331",
+    courseName: "Fundamental Fundamental Fundementaling",
+  },
+  {
+    request: "6",
+    courseCode: "CS 331",
+    courseName: "Fundamental Fundamental Fundementaling",
+  },
 ];
 
 export default function BasicTable() {
-    const [rows, setRows] = useState(originalRows);
-    const [searched, setSearched] = useState("");
-    const classes = useStyles();
+  const [rows, setRows] = useState(originalRows);
+  const [searched, setSearched] = useState("");
+  const classes = useStyles();
 
-    const requestSearch = (searchedVal: string) => {
-        const filteredRows = originalRows.filter((row) => {
-            return row.name.toLowerCase().includes(searchedVal.toLowerCase());
-        });
-        setRows(filteredRows);
-    };
+  const requestSearch = (searchedVal: string) => {
+    const filteredRows = originalRows.filter((row) => {
+      return row.name.toLowerCase().includes(searchedVal.toLowerCase());
+    });
+    setRows(filteredRows);
+  };
 
-    const cancelSearch = () => {
-        setSearched("");
-        requestSearch(searched);
-    };
+  const cancelSearch = () => {
+    setSearched("");
+    requestSearch(searched);
+  };
 
-    return (
-        <>
-            <Paper>
-                <SearchBar
-                    value={searched}
-                    onChange={(searchVal) => requestSearch(searchVal)}
-                    onCancelSearch={() => cancelSearch()}
-                />
-                <TableContainer>
-                    <Table className={classes.table} aria-label="simple table">
-                        <TableHead>
-                            <TableRow>
-
-                                <TableCell align="right">Request No</TableCell>
-                                <TableCell align="right">Course Code</TableCell>
-                                <TableCell align="right">Course Name</TableCell>
-
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {rows.map((row) => (
-                                <TableRow key={row.name}>
-                                    <TableCell component="th" scope="row">
-                                        {row.name}
-                                    </TableCell>
-                                    <TableCell align="right">{row.calories}</TableCell>
-                                    <TableCell align="right">{row.fat}</TableCell>
-                                    <TableCell align="right">{row.carbs}</TableCell>
-                                    <TableCell align="right"><button>Review Nomination Request</button></TableCell>
-                                </TableRow>
-                            ))}
-                        </TableBody>
-                    </Table>
-                </TableContainer>
-            </Paper>
-            <br />
-
-        </>
-    );
+  return (
+    <>
+      <Paper>
+        <SearchBar
+          value={searched}
+          onChange={(searchVal) => requestSearch(searchVal)}
+          onCancelSearch={() => cancelSearch()}
+        />
+        <TableContainer>
+          <Table className={classes.table} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell align="left">Request No</TableCell>
+                <TableCell align="left">Course Code</TableCell>
+                <TableCell align="left">Course Name</TableCell>
+                <TableCell align="left"></TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map((row, idx) => (
+                <TableRow key={idx}>
+                  <TableCell component="th" scope="row">
+                    {idx + 1}
+                  </TableCell>
+                  <TableCell align="left">{row.courseCode}</TableCell>
+                  <TableCell align="left">{row.courseName}</TableCell>
+                  <TableCell align="left">
+                    <button>Review Nomination Request</button>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Paper>
+      <br />
+    </>
+  );
 }
